8cc2cabc3e1c3610fa5c5b7a0d3138221ccbd3bf
Ninjanarwhal41 <88503808+Ninjanarwhal41@users.noreply.github.com>
2025-11-05 21:54:49 +0000

    Add files via upload
    updated final project

--- final.py
+++ final.py
@@ -1,36 +1,59 @@
 import RPi.GPIO as GPIO     ###various modules for speech-to-text, text-to-speech, lcd screen,and GPIO 
 import time
+import sys 
 import os
-os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+import lmstudio as lms
 import random
 import whisper #stt
 import speech_recognition as sr #stt
 import openai #stt
 import sounddevice 
+import tkinter as tk 
 from gtts import gTTS #tts
 from rpi_lcd import LCD #lcd screen
 from io import BytesIO
 from pygame import mixer #sound output from USB speaker 
 
-MIC_PIN = 26
-
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BCM) #GPIO and LCD setup
 GPIO.setwarnings(False)
-GPIO.setup(MIC_PIN, GPIO.IN) #setting up microphone input on pin 26 
 lcd = LCD()
 lcd.clear()
-text_output = "hello world, hello world! hello world."
 
+root = tk.Tk() #establishing main GUI window
+root.title("DUCK")
+frame_count = 12 # frames in my amazing gif
+#root.configure(background = "yellow")
+root.maxsize(1000, 1000)
+root.minsize(250, 250)
+root.geometry("600x600+0+300")
+tk.Label(root, text="It's ducky time").pack()
+tk.Label(root, text="Booting up...").pack()
+frames = [tk.PhotoImage(file='peak2.gif',format = 'gif -index %i' %(i)) for i in range(frame_count)]
 
+def update(ind):
+    frame = frames[ind]
+    ind += 1
+    if ind >= frame_count:  # With this condition it will play gif infinitely
+        ind = 0
+    label.configure(image=frame)
+    root.after(100, update, ind)
+label = tk.Label(root)
+label.pack()
+root.after(0, update, 0)
+root.mainloop()
+
+SERVER_API_HOST = "192.168.110.98:1234"
+lms.configure_default_client(SERVER_API_HOST)
+if lms.Client.is_valid_api_host(SERVER_API_HOST):
+    print(f"An LM Studio API server instance is available at {SERVER_API_HOST}")
+else:
+    print(f"No LM Studio API server instance found at {SERVER_API_HOST}")
+ai = lms.llm("mistralai/mistral-7b-instruct-v0.3")
 model = whisper.load_model("tiny.en")
 recognizer = sr.Recognizer()
 
 AM = open("hate.txt", 'r')
 script = AM.read()
-bee = open("bee.txt", 'r')
-script2 = bee.read()
-walter = open("walter.txt", 'r')
-script3 = walter.read()
 
 def lcd_text(txt, spd):
     chunk = 0
@@ -51,10 +74,7 @@ def lcd_text(txt, spd):
             l = 2
         else:
             l = 1 
-
-
-            
-    #goal is to create a scrolling effect.
+    #creates a scrolling effect
     time.sleep(spd * 10) 
     lcd.clear() 
 
@@ -81,6 +101,7 @@ def process(txt):
     tts = gTTS(txt, lang='en', tld='co.uk')
     tts.write_to_fp(mp3_fp)
     return mp3_fp
+
 def speak(txt):
     mixer.init()
     print("Processing...")
@@ -91,7 +112,27 @@ def speak(txt):
     mixer.music.play()
     time.sleep(60)
 
-speech = listen()
-print("You said:", speech)
-lcd_text(f"You said: {speech}", 0.05)
-speak(f"You said: {speech}")
+def main():
+    while True:
+        try:
+            ans = str(input("Would you like to activate DUCK? y for yes, n for no. \n You may exit at any time using Ctrl+D.\n "))
+            if ans.lower().strip() == 'y':
+                lcd_text("Hello world. Do not fear, for the DUCK is here!", 0.10)
+                speech = listen()
+                response = str(ai.respond(speech))
+                #root.mainloop()
+                #tk.Label(root, text="ready!")
+                #tk.Label(root, text=response).pack()
+                print("You said:", speech)
+                print("Response:", response)
+                lcd_text(f"You said: {speech} and I say {response}", 0.05)
+                speak(f"You said: {speech} and I say {response}")
+            else:
+                sys.exit("See ya later, alligator!")
+        except ValueError:
+            continue
+        except EOFError:
+            sys.exit("Thank you for using DUCK. Until next time!")
+
+if __name__ == "__main__":
+    main()
